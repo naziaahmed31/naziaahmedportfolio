630229e529025b0ad653590d8c5827ce41557907
naziaahmed31 <nazianahmed31@gmail.com>
2021-07-28 20:51:35 +0530

    myportfolio

---
+++ js/naziaportfolio.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+
+  // Toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
+
+var loader = document.querySelector(".loading");
+window.addEventListener("load", vanish);
+function vanish() {
+  loader.classList.add("disapper");
+}
